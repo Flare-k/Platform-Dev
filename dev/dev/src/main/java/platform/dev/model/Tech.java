18eec2d5b42872d7f6b10dc2c215102ca38ae550
rokkyw <rokkyw@naver.com>
2021-12-17 14:54:17 +0900

    Mapping USER and POST

---
+++ dev/dev/src/main/java/platform/dev/model/Tech.java
@@ -0,0 +1,2 @@
+package platform.dev.model;public class Tech {
+}
--- dev/dev/src/main/java/platform/dev/model/User.java
+++ dev/dev/src/main/java/platform/dev/model/User.java
@@ -1,13 +1,15 @@
 package platform.dev.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -18,23 +20,34 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
+
+    @Column(length = 100, nullable = false, unique = true)
     private String email;
+
     private String name;
+
+    @Size(min=2, max=10)
+    @NotBlank
+    @Column(nullable = false, unique = true)
     private String nickname;
+
+    @Size(min=5, max=20)
+    @NotBlank
+    @Column(nullable = false)
     private String password;
+
     private String address;
 
-    public User(
-            String email,
-            String name,
-            String nickname,
-            String password,
-            String address
-    ) {
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    @JsonIgnoreProperties({"user"})
+    private List<Post> postList;
+
+    public User(String email, String name, String nickname, String password, String address) {
         this.email = email;
         this.name = name;
         this.nickname = nickname;
         this.password = password;
         this.address = address;
     }
+
 }
